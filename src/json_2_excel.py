ee4d850b444dedcc0a0d31249c7001eb8c14dbc4
Zachary Trulby-Wright <zach.trulby-wright@cyberark.com>
2023-03-10 21:51:15 +0000

    First commit. Pushing entire project directory.

---
+++ src/json_2_excel.py
@@ -0,0 +1,78 @@
+# Importing classes from collections pandas, json, datetime
+from pandas import json_normalize
+import json
+import pandas as pd
+import datetime
+
+def convertXlsx(source_name="host_resources.json"):
+    # Read JSON file and write out EXCEL file
+    with open(source_name, 'r') as f:
+        data = json.loads(f.read())
+    
+    # Create DataFrame df1 for resource path resources:permissions with hostid column preserved in table header
+    df1 = pd.json_normalize(
+        data,
+        record_path='resources',
+        meta=['hostid'],
+        errors='ignore'
+    )
+    
+    # Drop unnecessary columns from JSON response in DataFrame df1
+    df1 = df1.drop(
+        columns=['replication_sets', 'policy_versions', 'annotations', 'permissions', 'secrets'],
+        errors='ignore'
+    )
+    
+    # Rename columns to descriptive label of data
+    df1 = df1.rename(
+        columns={"id": "resourceid"}
+    )
+    
+    # Create DataFrame df2 for resource path resources:permissions with hostid column preserved in table header
+    df2 = pd.json_normalize(
+        data,
+        record_path=['resources', 'permissions'],
+        meta=['hostid', ['resource', 'id']],
+        sep='',
+        errors='ignore'
+    )
+    
+    # Drop unnecessary columns from JSON response in DataFrame df2
+    df2 = df2.drop(
+        columns=['replication_sets', 'policy']
+    )
+    
+    # Rename columns to descriptive label of data
+    df2 = df2.rename(
+        columns={"hostid": "member ( hostid )", "role": "role ( groupid | hostid )"}
+    )
+    
+    # Create pivot table of resource:permissions
+    df3 = df2.drop(
+        columns=['role ( groupid | hostid )']
+    )
+    df3 = df3.pivot_table(
+        index = ["member ( hostid )", "resourceid", "privilege"],
+        values = ["member ( hostid )", "resourceid", "privilege"]
+    )
+    
+    # Current date object for output .xlsx file
+    current_date = datetime.date.today()
+    
+    # Write DataFrames df1, df2, df3 out to Excel-based output
+    with pd.ExcelWriter(str(current_date) + '_resources-by-apphost.xlsx') as writer:
+        df1.to_excel(writer, sheet_name="resources by hostid", index=False)
+        for column in df1:
+            column_length = max(df1[column].astype(str).map(len).max(), len(column))
+            col_idx = df1.columns.get_loc(column)
+            writer.sheets['resources by hostid'].set_column(col_idx, col_idx, column_length)
+        df2.to_excel(writer, sheet_name="permissions", index=False)
+        for column in df2:
+            column_length = max(df2[column].astype(str).map(len).max(), len(column))
+            col_idx = df2.columns.get_loc(column)
+            writer.sheets['permissions'].set_column(col_idx, col_idx, column_length)
+        df3.to_excel(writer, sheet_name="permissions by hostid")
+
+def __main__():
+    if __name__ == "__main__":
+        convertXlsx
---
+++ src/resourceByHost.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+# Preprocessing args from python subprocess call
+
+# Function initArg()
+# Args:
+#   -u --> Conjur URL
+#   -a --> Conjur account name
+#   -s --> Privileged service account running script
+#   -p --> Conjur audit-role password
+initArg(){
+    while getopts ":u:a:s:p:" flag
+    do
+        case "${flag}" in
+            u) url=${OPTARG};;
+            a) acct=${OPTARG};;
+            s) usr=${OPTARG};;
+            p) pass=${OPTARG};;
+        esac
+    done
+}
+
+# Call the function to parse the arguments from the command
+initArg $@
+
+# Preprocessing environment vars
+creds=$(echo "admin:$pass" | tr -d '\n' | base64)
+
+# openssl command to get public certificate back from Conjur
+openssl s_client -connect $url:443 \
+  -showcerts </dev/null 2> /dev/null | \
+  awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/ {print $0}' \
+  > /home/$usr/conjur.pem
+
+# INSTALL: jq for formatting output
+# sudo yum install jq -y
+
+# GET: API key for admin user
+apikey=$(curl --cacert /home/$usr/conjur.pem --silent --location --request GET "https://$url/authn/$acct/login" -H "Authorization: Basic $creds" --data-raw "")
+
+# POST: Auth-Z token for admin user
+token=$(curl --cacert /home/$usr/conjur.pem --silent --location --request POST "https://$url/authn/$acct/admin/authenticate" -H 'Content-Type: application/json' -H 'Accept-Encoding: base64' --data-raw "$apikey")
+
+# GET: Conjur hosts and send to file
+curl --cacert /home/$usr/conjur.pem --silent --location --request GET "https://$url/resources/$acct?kind=host" -H "Authorization: Token token=\"$token\"" > tmp.json
+
+cat tmp.json | grep -o '"id":"[^"]*' > tmp2.json
+cat tmp2.json | grep -o "$acct[^\"]*" > decode.json
+rm tmp.json tmp2.json
+
+# GET: Resources for each given host -> output results to output.file
+LINES=$(cat decode.json)
+echo "[" > hosts.json
+for LINE in $LINES
+do
+	echo $LINE > string.txt
+	sed -e 's/\//%2F/g' string.txt > string2.txt
+	sed -e 's/\:/%3A/g' string2.txt > string3.txt
+	cat string3.txt >> encoded.json
+	hostId=$(cat string3.txt)
+	host=$(cat string.txt)
+	
+	echo "{\"hostid\": "\"$host\""," >> hosts.json
+        echo "\"resources\": [" >> hosts.json
+       	resources="$(curl --cacert /home/$usr/conjur.pem --silent --request GET "https://$url/resources/$acct?role=$hostId" -H "Authorization: Token token=\"$token\"")"
+	resources=$(echo $resources | sed 's/^\[//g')
+	echo $resources | sed 's/]$//g' >> hosts.json
+	echo "]}," >> hosts.json
+done
+echo "]" >> hosts.json
+cat hosts.json | sed -z 's/},\n]/}\n]/g' > hosts2.json
+mv hosts2.json hosts.json
+cat hosts.json | jq '.' > "host_resources.tmp"
+rm -f *.txt *.json
+mv "host_resources.tmp" "host_resources.json"
